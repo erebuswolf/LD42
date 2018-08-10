cf2e0f4f93bbd56351d1930c66ebb8f4cb3c4359
Jesse Fish <jesfish@gmail.com>
2018-08-10 00:41:20 -0700

    Added input abstraction
    Added a simple input abstraction to unity to allow for functions to be assigned to key up down and held events.

---
+++ Assets/InputContainer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class InputContainer {
+    public Action OnKeyUp { get; private set; }
+    public Action OnKeyDown { get; private set; }
+    public Action OnKeyHeld { get; private set; }
+
+    public InputContainer() {
+
+    }
+
+    public InputContainer(Action onKeyDown, Action onKeyHeld, Action onKeyUp) {
+        this.OnKeyUp = onKeyUp;
+        this.OnKeyDown = onKeyDown;
+        this.OnKeyHeld = onKeyHeld;
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private InputAbstraction inputAbstraction;
+
+    // Use this for initialization
+    void Start () {
+        inputAbstraction = GetComponent<InputAbstraction>();
+        if (inputAbstraction ==null) {
+            Debug.LogError("Player Controller created with no input abstraction");
+        }
+        inputAbstraction.AddInputToAction(KeyCode.Space, new InputContainer(OnSpaceDown, OnSpaceHeld, OnSpaceUp));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnSpaceUp() {
+        Debug.LogWarning("space up");
+    }
+
+    public void OnSpaceDown() {
+        Debug.LogWarning("space down");
+    }
+
+    public void OnSpaceHeld() {
+        Debug.LogWarning("space held");
+    }
+}
