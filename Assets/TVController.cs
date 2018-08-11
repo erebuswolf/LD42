73fef908dcb04258f43ad365655bbd26f719e10b
Jesse Fish <jesfish@gmail.com>
2018-08-11 00:19:55 -0700

    fixed tv issues
    Fixed tv issues and halfway through recording first tv show.

--- Assets/TVController.cs
+++ Assets/TVController.cs
@@ -3,23 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TVController : MonoBehaviour {
-    private Animator animator;
+    private Animator tVAnimator;
 
+    [SerializeField]
+    private Animator VCRAnimator;
+
+    private bool isOn;
+
+    private bool VCRisOn;
+    
     // Use this for initialization
     void Start () {
-        animator = GetComponent<Animator>();
-        if (animator == null) {
+        tVAnimator = GetComponent<Animator>();
+        if (tVAnimator == null) {
             Debug.LogError("Null animator in TVController");
         }
     }
 	
     public void ToggleTV() {
-        animator.SetTrigger("ToggleOnOff");
+        isOn = !isOn;
+        tVAnimator.SetBool("TV", isOn);
         Debug.LogWarning("button pressed");
     }
     
+    public void TurnOnVCR() {
+        VCRisOn = !VCRisOn;
+        VCRAnimator.SetBool("VCR", VCRisOn);
+    }
+
+    public float GetAnimationTime() {
+        var info = VCRAnimator.GetCurrentAnimatorStateInfo(0);
+        return info.normalizedTime * info.length;
+    }
+
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+    }
 }
