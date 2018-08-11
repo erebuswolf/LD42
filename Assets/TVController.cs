042fc11dff5b87e555ae3b420bd00e76b03bb30e
Jesse Fish <jesfish@gmail.com>
2018-08-10 20:38:49 -0700

    TV turns on and off with sound
    Got a tv that is turning on and off with sound!

---
+++ Assets/TVController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TVController : MonoBehaviour {
+    private Animator animator;
+
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+        if (animator == null) {
+            Debug.LogError("Null animator in TVController");
+        }
+    }
+	
+    public void ToggleTV() {
+        animator.SetTrigger("ToggleOnOff");
+        Debug.LogWarning("button pressed");
+    }
+    
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
